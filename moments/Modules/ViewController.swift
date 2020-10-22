ba76be225b24b97bdf3eef79ab074eb26af37648
Jáder Nunes <jadernunes.jbn@gmail.com>
2020-10-22 19:00:00 -0300

    Added base xcode project

---
+++ moments/Modules/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  moments
+//
+//  Created by Jader Borba Nunes on 22/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
